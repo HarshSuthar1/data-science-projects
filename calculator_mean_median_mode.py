67b1fbba6a5262e8e235daaac78e21e86641e8d7
HarshSuthar1 <71977679+HarshSuthar1@users.noreply.github.com>
2025-05-23 14:07:54 +0000

    calculator

---
+++ calculator_mean_median_mode.py
@@ -0,0 +1,32 @@
+def mean(x):
+    return sum(x)/len(x)
+
+def median(x):
+    sorted_x = sorted(x)
+    if len(x)%2==1:
+        return sorted_x[len(x)//2]
+    else:
+        return (sorted_x[len(x)//2] + sorted_x[(len(x)//2)-1]) / 2
+
+def mode(x):
+    return max(x)
+
+length = int(input("Enter how many numbers u want: "))
+
+data = []
+
+print("Enter data: ")
+
+for i in range(length):
+    data.append(int(input("")))
+
+choice = int(input('''1. Mean
+2. Median
+3. Mode\n'''))
+
+if choice == 1:
+    print(mean(data))
+if choice == 2:
+    print(median(data))
+if choice == 3:
+    print(mode(data))
