4c87df825238de8384ca7fadb2142e103512ff18
HarshSuthar1 <71977679+HarshSuthar1@users.noreply.github.com>
2025-07-01 12:07:46 +0000

    Huge Changes Regarding Organising

--- Practicing/file_operation.py
+++ Practicing/file_operation.py
@@ -2,4 +2,4 @@ with open("test.txt",'r') as file:
     print(file.read())
 
 with open("test1.txt",'w') as file2:
-    file2.write("This is his brother, slave.")
+    file2.write("This is his brother, clay.")
